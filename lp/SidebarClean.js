65872dd063679a10bf77df12ea581d5ba960b892
Acid-Crash <32600318+Acid-Crash@users.noreply.github.com>
2024-04-25 12:26:40 +0300

    Update SidebarClean.js

--- lp/SidebarClean.js
+++ lp/SidebarClean.js
@@ -8,6 +8,8 @@
 		Lampa.Listener.follow('app',(e)=>{
         if(e.type == 'ready'){
 			setTimeout(function(){
+				$("[data-action=relise]").eq(0).remove();
+				$("[data-action=anime]").eq(0).remove();
 				$("[data-action=mytorrents]").eq(0).remove();
 				$("[data-action=feed]").eq(0).remove();
 			},10); 
