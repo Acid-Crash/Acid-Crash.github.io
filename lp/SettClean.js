b707030fd14a3c089e80a575e215e754e11840e3
Acid-Crash <32600318+Acid-Crash@users.noreply.github.com>
2024-04-28 15:22:40 +0300

    Update SettClean.js

--- lp/SettClean.js
+++ lp/SettClean.js
@@ -1,17 +1,8 @@
 (function () {
-    'use strict';	
-   var garbage_collector = {
-      name: 'SettingsCleanup',
-      version: '1.0',
-      description: 'Remove items from Settings'
-    };
-	Lampa.Listener.follow('full',(e)=>{
-        if(e.type == 'complite'){
-			setTimeout(function(){
-				$("[data-component=parser]").eq(0).remove();
-				$("[data-component=server]").eq(0).remove();
-			},10); 
-        }
-    });
+	'use strict';
+
+	window.lampa_settings.torrents_use = false;
+	window.lampa_settings.demo = false;
+	window.lampa_settings.read_only = false;
 
 })();
