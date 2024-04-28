3061f6e4e217490f2b9b9eefc7544e8416c47c58
Acid-Crash <32600318+Acid-Crash@users.noreply.github.com>
2024-04-28 15:37:30 +0300

    Update SettClean.js

--- lp/SettClean.js
+++ lp/SettClean.js
@@ -1,6 +1,9 @@
-(function () {
-	'use strict';
-
-	window.lampa_settings.torrents_use = false;
-
-})();
+Lampa.Settings.listener.follow('open', function(e) { 
+     if (e.name == 'main' { 
+           setTimeout(function() { 
+$('div[data-component="parental_control"]').remove();    
+$('div[data-component="server"]').remove();    
+$('div[data-component="parser"]').remove();     
+}, 0)  
+ } 
+});
