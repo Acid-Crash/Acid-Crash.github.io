66f680c5194b628f093c1d4c323c566dfe90c52c
Acid-Crash <32600318+Acid-Crash@users.noreply.github.com>
2024-04-30 21:46:26 +0300

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,12 @@
+(function () {
+   'use strict';
+   Lampa.Settings.listener.follow('open', function(e) {
+      if (e.name == 'main' {
+         setTimeout(function() {
+           $('div[data-component="parental_control"]').remove();
+           $('div[data-component="server"]').remove();
+           $('div[data-component="parser"]').remove();
+         },0);
+      });
+   });
+})();
