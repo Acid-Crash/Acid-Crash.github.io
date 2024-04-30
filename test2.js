6df5d85814733b1c7e3848b3557727f244f4dcfa
Acid-Crash <32600318+Acid-Crash@users.noreply.github.com>
2024-04-30 22:03:22 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,12 +1,13 @@
 (function () {
+    'use strict';	
 
-      Lampa.Settings.listener.follow('open',(e)=>{
+		Lampa.Settings.listener.follow('open',(e)=>{
         if(e.type == 'main'){
-         setTimeout(function(){
+			setTimeout(function(){
         $('div[data-component="parental_control"]').remove();
         $('div[data-component="server"]').remove();
         $('div[data-component="parser"]').remove();
-         },0);
+			},0); 
         }
     });
 
